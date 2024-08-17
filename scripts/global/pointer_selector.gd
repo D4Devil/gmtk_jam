03634c3642bc275f37a08abf880b49f09ac57a94
D4Devil <dmdio1177@gmail.com>
2024-08-16 23:08:55 -0600

    Added support for collision mask, configured collision layer 2 as clickable. Now the pointer will only collide with the clickable objects

--- scripts/global/pointer_selector.gd
+++ scripts/global/pointer_selector.gd
@@ -1,8 +1,8 @@
-class_name Pointer
 extends Node3D
 
 const ON_OBJECT_CLICKED := "on_object_clicked"
 const ON_OBJECT_DROPPED := "on_object_dropped"
+const CLICKABLE_COLLISION_MASK := 2
 
 ## Cached object
 var last_object_clicked: Node3D
@@ -30,7 +30,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		
 
 func select_world_object() -> void:
-	var collision_result : Dictionary = Utils.screen_to_3d_object(self)
+	var collision_result : Dictionary = Utils.screen_to_3d_object(self, CLICKABLE_COLLISION_MASK)
 
 	## Unload last object from signal
 	if last_object_clicked && last_object_clicked.has_method(ON_OBJECT_CLICKED):
@@ -55,10 +55,17 @@ func select_world_object() -> void:
 
 
 func drop_world_object() -> void:
+	## Guard against null
+	if not last_object_clicked:
+		return
+
 	emit_signal("object_dropped")
 
 	## Unwire the clickable interface
-	object_clicked.disconnect(last_object_clicked.on_object_clicked)
-	object_dropped.disconnect(last_object_clicked.on_object_dropped)
+	if last_object_clicked.has_method(ON_OBJECT_CLICKED):
+		object_clicked.disconnect(last_object_clicked.on_object_clicked)
+
+	if last_object_clicked.has_method(ON_OBJECT_DROPPED):
+		object_dropped.disconnect(last_object_clicked.on_object_dropped)
 
 	last_object_clicked = null
--- scripts/global/utils.gd
+++ scripts/global/utils.gd
@@ -1,12 +1,12 @@
 class_name Utils
 
 ## Gets the mouse position and projects it to the game world. returns collision data
-static func screen_to_3d_object(node: Node3D) -> Dictionary:
+static func screen_to_3d_object(node: Node3D, collision_mask: int = 4294967295) -> Dictionary:
 	var mouse_position = node.get_viewport().get_mouse_position()
 	var cam = node.get_viewport().get_camera_3d()
 	var origin = cam.project_ray_origin(mouse_position)
 	var dir = cam.project_ray_normal(mouse_position) * 100
-	var query = PhysicsRayQueryParameters3D.create(origin, dir)
+	var query = PhysicsRayQueryParameters3D.create(origin, dir, collision_mask)
 	
 	## Query the 3d direct space state, retrives a dictionary with collision
 	return node.get_world_3d().direct_space_state.intersect_ray(query)
