9185a0ac800e0d5b803dc75a9d30cb80ec3aa864
Xkonti <xkonti@xkonti.tech>
2024-08-17 00:16:29 -0400

    Reset timer automatically, make it automatically switch to results

--- scripts/global/growth_timer.gd
+++ scripts/global/growth_timer.gd
@@ -7,14 +7,25 @@ var time_left: float = total_time
 
 signal timeout()
 
+
 func _ready():
 	time_left = total_time
+	GameStateMachine.state_changed.connect(on_game_state_changed)
+
 
 func _process(delta):
 	time_left -= delta
 	if (time_left <= 0.0):
 		time_left = 0
 		timeout.emit()
+		if GameStateMachine.state == GameStateMachine.GameState.GROWTH:
+			GameStateMachine.on_growth_finished()
+
 
 func reset():
 	time_left = total_time
+
+
+func on_game_state_changed(_old_state: GameStateMachine.GameState, new_state: GameStateMachine.GameState):
+	if new_state == GameStateMachine.GameState.GROWTH:
+		reset()
