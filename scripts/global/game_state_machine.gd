99bd638d12672ea7f2d606b56ee3d25d52eac0ea
Xkonti <xkonti@xkonti.tech>
2024-08-17 00:25:38 -0400

    Mock up main menu, and create game state machine (#9)
    * Rename Player to Plant and make it Node3D
    
    * Create placeholder mesh and camera
    
    * Fix plant scene and script
    
    * Create basic plant stats autloaded script
    
    * Change World into Node3D
    
    * Make maid scene Node3D
    
    * Make temporary world
    
    * Make plant visually grow
    
    * Make kale green
    
    * Create progress bar border asset
    
    * Create custom progress bar component
    
    * Create basic plant stats ui scene
    
    * Create growth timer and UI for it
    
    * Fix scene issues
    
    * Add new game button
    
    * Define basic game state machine
    
    * Add GameStateMachine to autoloads
    
    * Hook up the start game to GameStateMachine
    
    * Connect start game btn signal
    
    * Make UI manage itself via state
    
    * Reset timer automatically, make it automatically switch to results

---
+++ scripts/global/game_state_machine.gd
@@ -0,0 +1,44 @@
+extends Node
+
+enum GameState {MAIN_MENU, GROWTH, RESULTS}
+
+var state: GameState = GameState.MAIN_MENU
+
+signal state_changed(old_state: GameState, new_state: GameState)
+
+func on_new_game_started():
+	if state != GameState.MAIN_MENU:
+		push_error("Tried to start new game while not in the main menu")
+		return
+	
+	print("Moving from MAIN_MENU to GROWTH state")
+	state = GameState.GROWTH
+	state_changed.emit(GameState.MAIN_MENU, state)
+
+func on_growth_finished():
+	if state != GameState.GROWTH:
+		push_error("Tried to finish growth while not in the growth stage")
+		return
+	
+	print("Moving from GROWTH to RESULTS state")
+	state = GameState.RESULTS
+	state_changed.emit(GameState.GROWTH, state)
+
+func on_results_finished():
+	if state != GameState.RESULTS:
+		push_error("Tried to finish results while not in the results stage")
+		return
+	
+	print("Moving from RESULTS to GROWTH state")
+	state = GameState.GROWTH
+	state_changed.emit(GameState.RESULTS, state)
+
+func on_stop_game():
+	if state == GameState.MAIN_MENU:
+		push_error("Tried to finish playing while not main menu")
+		return
+	
+	print("Moving to MAIN_MENU state")
+	var prev_state = state
+	state = GameState.MAIN_MENU
+	state_changed.emit(prev_state, state)
--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -2,3 +2,6 @@ extends Control
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+func _on_start_button_pressed():
+	GameStateMachine.on_new_game_started()
