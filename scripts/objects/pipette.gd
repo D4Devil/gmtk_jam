bb2be329da9d4e79ba9cad2655dccf17af44ea08
D4Devil <dmdio1177@gmail.com>
2024-08-17 12:27:41 -0600

    Adding pipette watering capabilities

---
+++ scripts/objects/pipette.gd
@@ -0,0 +1,19 @@
+class_name Pipette
+extends CharacterBody3D
+
+
+@export var watering_power: float = 10
+@onready var origin := transform.origin
+
+
+func on_clickable_selected(collision: Dictionary) -> void:
+    print('Pipette selected')
+
+func on_clickable_dropped() -> void:
+    print('Pipette Dropped')
+
+
+func on_clickable_use(using: bool) -> void:
+    if using:
+        print('Drowning the plant >:)')
+        PlantStats.add_water_volume(watering_power)
