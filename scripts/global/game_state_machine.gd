44b08e1472944052c5d3b30db34efcd7b3c725e0
Xkonti <xkonti@xkonti.tech>
2024-08-16 23:58:16 -0400

    Hook up the start game to GameStateMachine

--- scripts/global/game_state_machine.gd
+++ scripts/global/game_state_machine.gd
@@ -6,7 +6,7 @@ var state: GameState = GameState.MAIN_MENU
 
 signal state_changed(old_state: GameState, new_state: GameState)
 
-func _on_new_game_started():
+func on_new_game_started():
 	if state != GameState.MAIN_MENU:
 		push_error("Tried to start new game while not in the main menu")
 		return
@@ -15,7 +15,7 @@ func _on_new_game_started():
 	state = GameState.GROWTH
 	state_changed.emit(GameState.MAIN_MENU, state)
 
-func _on_growth_finished():
+func on_growth_finished():
 	if state != GameState.GROWTH:
 		push_error("Tried to finish growth while not in the growth stage")
 		return
@@ -24,7 +24,7 @@ func _on_growth_finished():
 	state = GameState.RESULTS
 	state_changed.emit(GameState.GROWTH, state)
 
-func _on_results_finished():
+func on_results_finished():
 	if state != GameState.RESULTS:
 		push_error("Tried to finish results while not in the results stage")
 		return
@@ -33,7 +33,7 @@ func _on_results_finished():
 	state = GameState.GROWTH
 	state_changed.emit(GameState.RESULTS, state)
 
-func _on_stop_game():
+func on_stop_game():
 	if state == GameState.MAIN_MENU:
 		push_error("Tried to finish playing while not main menu")
 		return
