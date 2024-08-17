f6aa0dcf5be9ea98ceeaaade049139b7271b7df9
Xkonti <xkonti@xkonti.tech>
2024-08-17 11:58:54 -0400

    Import models, work on kale (#13)
    * Import 3D models
    
    * Import some textures
    
    * Apply kale materials
    
    * Replate kale model, add kale growth stage anim
    
    * Trigger growth animation at specified size threshold

--- scripts/characters/plant.gd
+++ scripts/characters/plant.gd
@@ -1,4 +1,21 @@
 extends Node3D
 
+## A plant size value when the model should switch from
+## seedling to fully grown.
+@export var full_grown_size: float = 1.0
+
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
+
+## Is the plant fully grown. Keeps track if the full grown animation has played yet.
+var is_full: bool = false
+
+
 func _process(_delta):
 	scale = Vector3.ONE * PlantStats.size
+	if not is_full and scale.x >= full_grown_size:
+		is_full = true
+		anim_player.play("grow_full")
+
+
+func start():
+	anim_player.play("start")
