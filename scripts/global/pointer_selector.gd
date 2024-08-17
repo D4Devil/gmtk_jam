f592a5af337cee45342934f35e42d685613ba498
D4Devil <dmdio1177@gmail.com>
2024-08-16 22:19:31 -0600

    Fixed tabbing

--- scripts/global/pointer_selector.gd
+++ scripts/global/pointer_selector.gd
@@ -10,34 +10,35 @@ signal object_clicked(collision: Dictionary)
 
 
 func _init() -> void:
-    PhysicsServer3D.set_active(true)
+	PhysicsServer3D.set_active(true)
 
 
 func _unhandled_input(event: InputEvent) -> void:
-    ## Guard against anything but the mouse buttons 
-    if event is InputEventMouseButton:
-        return
+	## Guard against anything but the mouse buttons 
+	if event is InputEventMouseButton:
+		return
 
-    if event.is_action_pressed("in_game_select"):
-        select_world_object()
+	if event.is_action_pressed("in_game_select"):
+		select_world_object()
+		
         
 
 func select_world_object() -> void:
-    var collision_result : Dictionary = Utils.screen_to_3d_object(self)
+	var collision_result : Dictionary = Utils.screen_to_3d_object(self)
 
-    ## Unload last object from signal
-    if last_object_clicked && last_object_clicked.has_method(ON_OBJECT_CLICKED):
-        object_clicked.disconnect(last_object_clicked.on_object_clicked)
+	## Unload last object from signal
+	if last_object_clicked && last_object_clicked.has_method(ON_OBJECT_CLICKED):
+		object_clicked.disconnect(last_object_clicked.on_object_clicked)
 
-    ## Process the current selected object
-    if collision_result.has("collider"):
-        var cached_collider = collision_result["collider"] as Node3D
+	## Process the current selected object
+	if collision_result.has("collider"):
+		var cached_collider = collision_result["collider"] as Node3D
 
-        if cached_collider == last_object_clicked:
-            return
+		if cached_collider == last_object_clicked:
+			return
 
-        last_object_clicked = cached_collider
+		last_object_clicked = cached_collider
 
-        if last_object_clicked.has_method(ON_OBJECT_CLICKED):
-            object_clicked.connect(last_object_clicked.on_object_clicked)
-            emit_signal("object_clicked", collision_result)
+		if last_object_clicked.has_method(ON_OBJECT_CLICKED):
+			object_clicked.connect(last_object_clicked.on_object_clicked)
+			emit_signal("object_clicked", collision_result)
