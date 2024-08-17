5201bebd383cc7a62c9e09057276cdf5e2a3769c
D4Devil <dmdio1177@gmail.com>
2024-08-16 22:17:15 -0600

    Fixed tabbing

--- scripts/global/pointer_selector.gd
+++ scripts/global/pointer_selector.gd
@@ -14,11 +14,15 @@ func _init() -> void:
 
 
 func _unhandled_input(event: InputEvent) -> void:
-    if event is InputEventMouseButton && event.is_action_pressed("in_game_select"):
-        get_world_object()
+    ## Guard against anything but the mouse buttons 
+    if event is InputEventMouseButton:
+        return
 
+    if event.is_action_pressed("in_game_select"):
+        select_world_object()
+        
 
-func get_world_object() -> void:
+func select_world_object() -> void:
     var collision_result : Dictionary = Utils.screen_to_3d_object(self)
 
     ## Unload last object from signal
@@ -27,9 +31,13 @@ func get_world_object() -> void:
 
     ## Process the current selected object
     if collision_result.has("collider"):
-        last_object_clicked = collision_result["collider"] as Node3D
+        var cached_collider = collision_result["collider"] as Node3D
+
+        if cached_collider == last_object_clicked:
+            return
+
+        last_object_clicked = cached_collider
 
         if last_object_clicked.has_method(ON_OBJECT_CLICKED):
             object_clicked.connect(last_object_clicked.on_object_clicked)
             emit_signal("object_clicked", collision_result)
-        
