a4eb8160726eeb623a3cc88afc747f71de215397
Xkonti <xkonti@xkonti.tech>
2024-08-17 00:15:46 -0400

    Make UI manage itself via state

--- scripts/ui/ui.gd
+++ scripts/ui/ui.gd
@@ -1,1 +1,28 @@
 extends CanvasLayer
+
+@onready var main_menu: Control = $MainMenu
+@onready var plant_stats_ui: Control = $PlantStatsUi
+@onready var timer_ui: Control = $TimerUI
+
+func _ready():
+	GameStateMachine.state_changed.connect(on_game_state_changed)
+
+
+func on_game_state_changed(old_state: GameStateMachine.GameState, new_state: GameStateMachine.GameState):
+	# MAIN MENU
+	if old_state == GameStateMachine.GameState.MAIN_MENU:
+		main_menu.visible = false
+
+	if new_state == GameStateMachine.GameState.MAIN_MENU:
+		main_menu.visible = true
+
+	# GROWTH
+	if old_state == GameStateMachine.GameState.GROWTH:
+		plant_stats_ui.visible = false
+		timer_ui.visible = false
+
+	if new_state == GameStateMachine.GameState.GROWTH:
+		plant_stats_ui.visible = true
+		timer_ui.visible = true
+
+	# RESULTS
