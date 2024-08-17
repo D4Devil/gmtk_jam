fa3321cd579b44e951012424a44983001ad9dc46
D4Devil <dmdio1177@gmail.com>
2024-08-16 22:48:02 -0600

    Added support for drop event

--- scripts/global/pointer_selector.gd
+++ scripts/global/pointer_selector.gd
@@ -2,11 +2,13 @@ class_name Pointer
 extends Node3D
 
 const ON_OBJECT_CLICKED := "on_object_clicked"
+const ON_OBJECT_DROPPED := "on_object_dropped"
 
 ## Cached object
 var last_object_clicked: Node3D
 
 signal object_clicked(collision: Dictionary)
+signal object_dropped()
 
 
 func _init() -> void:
@@ -15,13 +17,17 @@ func _init() -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	## Guard against anything but the mouse buttons 
-	if event is InputEventMouseButton:
+	if not event is InputEventMouseButton:
 		return
 
 	if event.is_action_pressed("in_game_select"):
 		select_world_object()
+		return
+
+	if event.is_action_pressed("in_game_drop"):
+		drop_world_object()
+		return
 		
-        
 
 func select_world_object() -> void:
 	var collision_result : Dictionary = Utils.screen_to_3d_object(self)
@@ -39,6 +45,20 @@ func select_world_object() -> void:
 
 		last_object_clicked = cached_collider
 
+		## Wire the clickable interface
 		if last_object_clicked.has_method(ON_OBJECT_CLICKED):
 			object_clicked.connect(last_object_clicked.on_object_clicked)
 			emit_signal("object_clicked", collision_result)
+
+		if last_object_clicked.has_method(ON_OBJECT_DROPPED):
+			object_dropped.connect(last_object_clicked.on_object_dropped)
+
+
+func drop_world_object() -> void:
+	emit_signal("object_dropped")
+
+	## Unwire the clickable interface
+	object_clicked.disconnect(last_object_clicked.on_object_clicked)
+	object_dropped.disconnect(last_object_clicked.on_object_dropped)
+
+	last_object_clicked = null
