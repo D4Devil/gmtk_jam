a1fb0cfc168ec3a1bfc0d844f59807ff20ee1cd4
Xkonti <xkonti@xkonti.tech>
2024-08-18 00:17:55 -0400

    Basic grading categories

--- scripts/ui/results_ui.gd
+++ scripts/ui/results_ui.gd
@@ -1,4 +1,7 @@
 extends Control
 
+## Categories must be ordered from smallest size to largest size
+@export var categories: Array[GradingCategory]
+
 func _on_restart_button_pressed():
 	GameStateMachine.on_results_finished()
