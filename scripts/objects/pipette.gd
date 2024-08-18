9be2ef3c045b1e48388573959feea2e616e7f627
D4Devil <dmdio1177@gmail.com>
2024-08-17 21:19:21 -0600

    Added dragging support

--- scripts/objects/pipette.gd
+++ scripts/objects/pipette.gd
@@ -1,20 +1,35 @@
 class_name Pipette
-extends CharacterBody3D
-
+extends RigidBody3D
 
 @export var watering_power: float = 1
-@onready var origin := transform.origin
+@export var drag_collision_layer := 16
+
+@onready var draggable := $DragAgent
+
+func _ready() -> void:
+    GameStateMachine.state_changed.connect(on_game_state_changed)
+    freeze_mode = FREEZE_MODE_KINEMATIC
+    freeze = true
+    draggable.enable = false
+    draggable.plane_mask = drag_collision_layer
 
 
-func on_clickable_selected(collision: Dictionary) -> void:
-    print('Pipette selected')
+func on_game_state_changed(_previous: GameStateMachine.GameState, next: GameStateMachine.GameState) -> void:
+    if next == GameStateMachine.GameState.GROWTH:
+        freeze = false
+        
+
+func on_clickable_selected(_collision: Dictionary) -> void:
+    freeze = true
+    draggable.enable = true
+    
 
 
 func on_clickable_dropped() -> void:
-    print('Pipette Dropped')
+    freeze = false
+    draggable.enable = false
 
 
 func on_clickable_use(using: bool) -> void:
     if using:
-        print('Drowning the plant >:)')
         PlantStats.add_water_volume(watering_power)
