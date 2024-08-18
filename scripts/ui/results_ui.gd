da69982729cd1960e02d3dc06daac47892e4919c
Xkonti <xkonti@xkonti.tech>
2024-08-18 02:01:25 -0400

    Implement and utilize grading

--- scripts/ui/results_ui.gd
+++ scripts/ui/results_ui.gd
@@ -3,5 +3,18 @@ extends Control
 ## Categories must be ordered from smallest size to largest size
 @export var categories: Array[GradingCategory]
 
+## How many places (participants) are there in the category
+@export var participants_per_category: int
+
+func _ready():
+	GradingManager.categories = categories
+	GradingManager.participants_per_category = participants_per_category
+	GradingManager.size_graded.connect(refresh_grading)
+
 func _on_restart_button_pressed():
 	GameStateMachine.on_results_finished()
+		
+
+## TODO: Update all the UI for displaying the category and place
+func refresh_grading():
+	pass
