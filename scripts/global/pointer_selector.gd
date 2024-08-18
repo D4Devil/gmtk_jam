c3bba6b0b61453a661cd1a3f8ba3caf606cbc617
D4Devil <dmdio1177@gmail.com>
2024-08-18 15:17:01 -0600

    Rafactored clickable, pickable and drag agent components and suport changes on pointer selector

--- scripts/global/pointer_selector.gd
+++ scripts/global/pointer_selector.gd
@@ -1,17 +1,20 @@
 extends Node3D
 
-const ON_CLICKABLE_SELECTED := "on_clickable_selected"
-const ON_CLICKABLE_DROPPED := "on_clickable_dropped"
-const ON_USING_CLICKABLE := "on_clickable_use"
+# const ON_CLICKABLE_SELECTED := "on_clickable_selected"
+# const ON_CLICKABLE_DROPPED := "on_clickable_dropped"
+# const ON_USING_CLICKABLE := "on_clickable_use"
 
 const CLICKABLE_COLLISION_MASK := 2
+const CLICKABLE_ACTION := "in_game_select"
 
-## Cached object
-var last_object_clicked: Node3D
+var enabled := true
 
-signal object_clicked(collision: Dictionary)
-signal clickable_use(using: bool)
-signal object_dropped()
+# ## Cached object
+# var last_object_clicked: Node3D
+
+# signal object_clicked(collision: Dictionary)
+# signal clickable_use(using: bool)
+# signal object_dropped()
 
 
 func _init() -> void:
@@ -20,94 +23,112 @@ func _init() -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	## Guard against anything but the mouse buttons 
-	if event is not InputEventMouseButton:
+	if not event.is_action_pressed(CLICKABLE_ACTION) or not enabled:
 		return
 
-	if event.is_action_pressed("in_game_select"):
-		if last_object_clicked:
-			start_using_clickable()
-		else:
-			get_clickable()
-		return
-
-	if event.is_action_released("in_game_select") and last_object_clicked:
-		stop_using_clickable()
-
-	if event.is_action_pressed("in_game_drop"):
-		drop_clickable()
-		return
-
-
-## Try to select an object at current's mouse position.
-func get_clickable() -> void:
 	var collision_result : Dictionary = Utils.screen_to_3d_object(self, CLICKABLE_COLLISION_MASK)
+	var bodyClicked: Node = collision_result.get("collider")
 
-	## TODO: If there is a parent ClickableObject, call the `on_clicked` on that parent.
-	var bodyClicked: Node
-	var parent_node = bodyClicked.get_parent()
-	if parent_node is ClickableObject:
-		# Don't select anything. Simply call parent_node.
+	if not bodyClicked:
+		return
+	
+	## Search for parent clickable
+	if bodyClicked.get_parent() is Clickable:
+		bodyClicked.get_parent().on_clicked()
 		return
 
-	## Unload last object from signal
-	if last_object_clicked && last_object_clicked.has_method(ON_CLICKABLE_SELECTED):
-		object_clicked.disconnect(last_object_clicked.on_object_clicked)
+	## Search for children clickable
+	var clickables := bodyClicked.find_children("*", "Clickable", false)
+	
+	for clickable in clickables:
+		assert(clickable is Clickable)
+		clickable.on_clicked()
 
-	## Process the current selected object
-	if collision_result.has("collider"):
-		var cached_collider = collision_result["collider"] as Node3D
+	# if event.is_action_pressed("in_game_select"):
+	# 	if last_object_clicked:
+	# 		start_using_clickable()
+	# 	else:
+	# 		get_clickable()
+	# 	return
 
-		if cached_collider == last_object_clicked:
-			return
+	# if event.is_action_released("in_game_select") and last_object_clicked:
+	# 	stop_using_clickable()
 
-		last_object_clicked = cached_collider
-		wire_clickable()
-
-		if object_clicked:
-			emit_signal("object_clicked", collision_result)
+	# if event.is_action_pressed("in_game_drop"):
+	# 	drop_clickable()
+	# 	return
 
 
-func wire_clickable():
-	if last_object_clicked.has_method(ON_CLICKABLE_SELECTED):
-		object_clicked.connect(last_object_clicked.on_clickable_selected)
+# ## Try to select an object at current's mouse position.
+# func get_clickable() -> void:
+# 	var collision_result : Dictionary = Utils.screen_to_3d_object(self, CLICKABLE_COLLISION_MASK)
+
+# 	## TODO: If there is a parent ClickableObject, call the `on_clicked` on that parent.
+# 	var bodyClicked: Node
+# 	var parent_node = bodyClicked.get_parent()
+# 	if parent_node is ClickableObject:
+# 		# Don't select anything. Simply call parent_node.
+# 		return
+
+# 	## Unload last object from signal
+# 	if last_object_clicked && last_object_clicked.has_method(ON_CLICKABLE_SELECTED):
+# 		object_clicked.disconnect(last_object_clicked.on_object_clicked)
+
+# 	## Process the current selected object
+# 	if collision_result.has("collider"):
+# 		var cached_collider = collision_result["collider"] as Node3D
+
+# 		if cached_collider == last_object_clicked:
+# 			return
+
+# 		last_object_clicked = cached_collider
+# 		wire_clickable()
+
+# 		if object_clicked:
+# 			emit_signal("object_clicked", collision_result)
+
+
+# func wire_clickable():
+# 	if last_object_clicked.has_method(ON_CLICKABLE_SELECTED):
+# 		object_clicked.connect(last_object_clicked.on_clickable_selected)
 		
-	if last_object_clicked.has_method(ON_USING_CLICKABLE):
-		clickable_use.connect(last_object_clicked.on_clickable_use)
+# 	if last_object_clicked.has_method(ON_USING_CLICKABLE):
+# 		clickable_use.connect(last_object_clicked.on_clickable_use)
 
-	if last_object_clicked.has_method(ON_CLICKABLE_DROPPED):
-		object_dropped.connect(last_object_clicked.on_clickable_dropped)
-
-
-func drop_clickable() -> void:
-	## Guard against null
-	if not last_object_clicked:
-		return
-
-	emit_signal("object_dropped")
-	unwire_clickable()
-	last_object_clicked = null
+# 	if last_object_clicked.has_method(ON_CLICKABLE_DROPPED):
+# 		object_dropped.connect(last_object_clicked.on_clickable_dropped)
 
 
-func unwire_clickable():
-	if last_object_clicked.has_method(ON_CLICKABLE_SELECTED):
-		object_clicked.disconnect(last_object_clicked.on_clickable_selected)
+# func drop_clickable() -> void:
+# 	## Guard against null
+# 	if not last_object_clicked:
+# 		return
 
-	if last_object_clicked.has_method(ON_USING_CLICKABLE):
-		clickable_use.disconnect(last_object_clicked.on_clickable_use)
-
-	if last_object_clicked.has_method(ON_CLICKABLE_DROPPED):
-		object_dropped.disconnect(last_object_clicked.on_clickable_dropped)
+# 	emit_signal("object_dropped")
+# 	unwire_clickable()
+# 	last_object_clicked = null
 
 
-func start_using_clickable():
-	if not clickable_use:
-		return
+# func unwire_clickable():
+# 	if last_object_clicked.has_method(ON_CLICKABLE_SELECTED):
+# 		object_clicked.disconnect(last_object_clicked.on_clickable_selected)
+
+# 	if last_object_clicked.has_method(ON_USING_CLICKABLE):
+# 		clickable_use.disconnect(last_object_clicked.on_clickable_use)
+
+# 	if last_object_clicked.has_method(ON_CLICKABLE_DROPPED):
+# 		object_dropped.disconnect(last_object_clicked.on_clickable_dropped)
+
+
+# func start_using_clickable():
+# 	if not clickable_use:
+# 		return
 	
-	emit_signal("clickable_use", true)
+# 	emit_signal("clickable_use", true)
 
 
-func stop_using_clickable():
-	if not clickable_use:
-		return
+# func stop_using_clickable():
+# 	if not clickable_use:
+# 		return
 	
-	emit_signal("clickable_use", false)
+# 	emit_signal("clickable_use", false)
--- scripts/objects/components/drag_agent.gd
+++ scripts/objects/components/drag_agent.gd
@@ -1,43 +1,35 @@
 class_name DragAgent
 extends Node3D
 
-## Turn the behavior on and off
-var enable := false
-
 @export var speed: float = 10
 
 ## Collision mask
-@export_flags("Pipette:16") var plane_mask := 0
+@export_flags("Closest:16","Floor:8") var plane_mask := 0
+@export var root: Node3D
 
-## Draggable physic object
-@export var physics_node: PhysicsBody3D
+var enable = false : set = set_enable
 
-## New position for the object
-var move_to := Vector3.ZERO
-var velocity := Vector3.ZERO
-
-
-func _unhandled_input(event: InputEvent) -> void:
-	## Guard against events unless is mouse motion
-	if event is not InputEventMouseMotion or not enable:
-		return
-
-	## NOTE:: This event is only triggered when the mouse moves
-	event = event as InputEventMouseMotion
-	var collisions = Utils.screen_to_3d_object(self, plane_mask)
-	if collisions.has("collider"):
-		move_to = collisions["position"]
+## Cached Values
+var _move_to := Vector3.ZERO
+var _velocity := Vector3.ZERO
 
 
 func _process(delta: float) -> void:
 	if not enable:
 		return
 
-	physics_node.look_at(Vector3.ZERO)
-	velocity = _sqrt_interpolation(delta) * speed
-	physics_node.transform.origin += velocity
+	var collisions = Utils.screen_to_3d_object(self, plane_mask)
+	if collisions.has("collider"):
+		_move_to = collisions["position"]
+
+	root.look_at(Vector3.ZERO)
+	_velocity = _sqrt_interpolation(delta) * speed
+	root.transform.origin += _velocity
 
 
 func _sqrt_interpolation(delta) -> Vector3:
-	return physics_node.global_position.direction_to(move_to) * physics_node.global_position.distance_squared_to(move_to) * delta
+	return root.global_position.direction_to(_move_to) * root.global_position.distance_squared_to(_move_to) * delta
 	
+
+func set_enable(value: bool) -> void:
+	enable = value
