858f2734f27694dc93fe8faaaef27213959a40b7
D4Devil <dmdio1177@gmail.com>
2024-08-19 10:54:23 -0600

    Squashed commit of the following:
    commit 745d850ed8fe48d7ad887e4dfee60ab1115decda
    Author: Xkonti <xkonti@xkonti.tech>
    Date:   Mon Aug 19 12:46:52 2024 -0400
    
        Implement growth ray - needs textures (#40)

--- scripts/characters/plant.gd
+++ scripts/characters/plant.gd
@@ -1,4 +1,9 @@
+class_name Plant
 extends Node3D
+
+static var plant_center: Vector3
+
+@onready var plant_center_marker: Marker3D = $PlantCenter
 
 ## A plant size value when the model should switch from
 ## seedling to fully grown.
@@ -12,6 +17,7 @@ var is_full: bool = false
 
 func _process(_delta):
 	scale = Vector3.ONE * PlantStats.size
+	plant_center = plant_center_marker.global_position
 	if not is_full and scale.x >= full_grown_size:
 		is_full = true
 		anim_player.play("grow_full")
