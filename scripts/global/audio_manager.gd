964ac1177fc54ef9c36e0ff870a43fafdf00a9d7
Xkonti <xkonti@xkonti.tech>
2024-08-19 17:34:12 -0400

    Wind sounds

--- scripts/global/audio_manager.gd
+++ scripts/global/audio_manager.gd
@@ -4,9 +4,15 @@ extends Node3D
 static var instance: AudioManager
 
 @export var tap_scene: PackedScene
+@export var wind_per_size: Curve
+
+@onready var wind: AudioStreamPlayer = $Wind
 
 func _ready():
 	instance = self
+
+func _process(delta):
+	wind.volume_db = wind_per_size.sample(PlantStats.size_sample_value)
 
 func tap_at(pos: Vector3):
 	var tap_node = tap_scene.instantiate()
