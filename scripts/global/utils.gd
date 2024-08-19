d125189971af501e9ddae92b609a7642a62d73c3
Xkonti <xkonti@xkonti.tech>
2024-08-19 10:56:27 -0400

    Fix clicking!

--- scripts/global/utils.gd
+++ scripts/global/utils.gd
@@ -5,8 +5,9 @@ static func screen_to_3d_object(node: Node3D, collision_mask: int = 4294967295) 
 	var mouse_position = node.get_viewport().get_mouse_position()
 	var cam = node.get_viewport().get_camera_3d()
 	var origin = cam.project_ray_origin(mouse_position)
-	var dir = cam.project_ray_normal(mouse_position) * 10000
-	var query = PhysicsRayQueryParameters3D.create(origin, dir, collision_mask)
+	var location_on_plane = cam.project_position(mouse_position, 100)
+	var dir = location_on_plane - origin
+	var query = PhysicsRayQueryParameters3D.create(origin, dir * 10, collision_mask)
 	
 	## Query the 3d direct space state, retrives a dictionary with collision
 	return node.get_world_3d().direct_space_state.intersect_ray(query)
--- scripts/objects/ladybug_2.gd
+++ scripts/objects/ladybug_2.gd
@@ -2,4 +2,4 @@ extends Node3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func on_click():
-	print("Oh my! Got clicked!")
+	queue_free()
