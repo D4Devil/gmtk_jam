0fdde0e76d5a5fa658fe1d80623a2d8ddfbb9001
Xkonti <xkonti@xkonti.tech>
2024-08-19 02:26:24 -0400

    Fertilizer stat + debug UI (#36)
    * Revamp plant stats UI
    
    * Add fertilizer stat and the debug ui
    
    * Fix merge issue
    
    * Fix ladybug
    
    * Update plant's collision
    
    * Create manhole
    
    * Increase ray distance
    
    * Desperate attempt at adding another ladybug

--- scripts/global/utils.gd
+++ scripts/global/utils.gd
@@ -5,7 +5,7 @@ static func screen_to_3d_object(node: Node3D, collision_mask: int = 4294967295) 
 	var mouse_position = node.get_viewport().get_mouse_position()
 	var cam = node.get_viewport().get_camera_3d()
 	var origin = cam.project_ray_origin(mouse_position)
-	var dir = cam.project_ray_normal(mouse_position) * 100
+	var dir = cam.project_ray_normal(mouse_position) * 10000
 	var query = PhysicsRayQueryParameters3D.create(origin, dir, collision_mask)
 	
 	## Query the 3d direct space state, retrives a dictionary with collision
---
+++ scripts/objects/ladybug_2.gd
@@ -0,0 +1,5 @@
+extends Node3D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func on_click():
+	print("Oh my! Got clicked!")
